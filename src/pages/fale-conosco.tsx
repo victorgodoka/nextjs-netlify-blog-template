bd65f9b9f165f92468c1ca36fa8de985a3493af7
William Ribeiro Martins <william.martins@Williams-MacBook-Pro.local>
2021-10-07 11:07:40 -0300

    create buttons fale-conosco

--- src/pages/fale-conosco.tsx
+++ src/pages/fale-conosco.tsx
@@ -6,6 +6,12 @@ const Card = styled.div`
   box-shadow: 0px 4.74877px 17.8079px rgba(240, 161, 180, 0.4);
   backdrop-filter: blur(124.655px);
   border-radius: 14.2463px;
+  font-size: 24px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
 
   &.reclamacao {
     background: radial-gradient(
@@ -35,9 +41,20 @@ const Card = styled.div`
 export default function FaleConosco() {
   return (
     <div>
-      <Card className="reclamacao">aaa</Card>
-      <Card className="sugestao">aaa</Card>
-      <Card className="duvida">aaa</Card>
+      <Card className="reclamacao">
+        <img src="/images/reclamacao-icon.svg"/>
+        <p>Reclamação</p>
+      </Card>
+
+      <Card className="sugestao">
+        <img src="/images/sugestao-icon.svg"/>
+        <p>Sugestão</p>
+      </Card>
+      
+      <Card className="duvida">
+        <img src="/images/duvida-icon.svg"/>
+        <p>Dúvida</p>
+      </Card>
     </div>
   );
 }
